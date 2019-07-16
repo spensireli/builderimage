6adae51564258ae6f6f557233df44bfa8ca5bb0b
Tim Savage <savagete@upmc.edu>
2019-07-16 10:16:49 -0400

    fix awscli

--- buildit.sh
+++ buildit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apk update
 apk add py-pip terraform git ansible curl docker jq openssh python3 python-dev libffi-dev openssl-dev build-base
-pip install awscli requests simplejson boto3 boto cffi pywinrm --upgrade --user
+pip install requests simplejson boto3 boto cffi pywinrm --upgrade --user
+pip3 install awscli
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/linux/amd64/aws-iam-authenticator
 mv aws-iam-authenticator /bin/
 chmod 755 /bin/aws-iam-authenticator
